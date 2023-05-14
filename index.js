13e14a5e9e9072ccbe1e1fde067ded50a5b28821
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-05-14 17:42:28 +0600

    added data to mongodb

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+// ruhulaminpollob701
+//StVZLT5RFBbJZxAo
 
-
-const uri = "mongodb+srv://ruhulaminpollob:otxUMwt1SPMgCmp@cluster0.uwz0znz.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://ruhulaminpollob701:StVZLT5RFBbJZxAo@cluster0.uwz0znz.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,12 +26,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const userCollection=client.db("usersDB").collection("user")
+
+    app.get("/users", async (req,res)=>{
+      
+    })
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      console.log('new user', user);
+
+      const result= await userCollection.insertOne(user)
+      res.send(result)
+      
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
