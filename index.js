4512f090e0fee175787926dff5237907209663d8
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-05-15 22:33:13 +0600

    delete users successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -41,7 +41,14 @@ async function run() {
 
       const result= await userCollection.insertOne(user)
       res.send(result)
-      
+    })
+    app.delete("/users/:id", async(req,res)=>{
+      const id=req.params.id
+      console.log("Please delete from database id",id);
+
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
     })
 
 
